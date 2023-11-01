5b471ccd7766860bae60bedd045b8e487fa9b52c
steno3 <73556479+steno3@users.noreply.github.com>
2023-11-01 17:56:28 +0100

    player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float _horizontal;
+    private float _vertical;
+    private bool InputDisabled;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!InputDisabled)
+        {
+            if (_horizontal != 0 && _vertical != 0)
+            {
+                rb.velocity = new Vector2(_horizontal * moveSpeed / 1.4f, _vertical * moveSpeed / 1.4f);
+            }
+            else
+            {
+                rb.velocity = new Vector2(_horizontal * moveSpeed, _vertical * moveSpeed);
+            }
+        }
     }
 }
