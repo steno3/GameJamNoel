4d0df4a59756944040a08b8c05f0a6679fca3397
Rugysa <150249479+Rugysa@users.noreply.github.com>
2023-12-11 17:29:15 +0100

    Ajout barre de chargement

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI ProgressText;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            //Debug.Log(progress);
+
+            ProgressText.text = progress*100 + "%";
+            yield return null;
+        }
+        
+    }
+}
